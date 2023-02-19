0df905e54ad94dcc39e5dc6d4b1e5e269b0be1b0
Abhinav Sharma <abhinavsharma@Abhinavs-MacBook-Air.local>
2023-02-19 17:53:12 +0530

    Testing

--- include/plex_model_impl.h
+++ include/plex_model_impl.h
@@ -37,7 +37,7 @@ void PlexModel<key_t>::train(const std::vector<key_t> &keys,const std::vector<si
         model_keys[i] = keys[i];
     }
     std::sort(model_keys.begin(),model_keys.end());
-    this->vec=&keys;
+    this->vec=&model_keys;
     
 
     // Build TS
